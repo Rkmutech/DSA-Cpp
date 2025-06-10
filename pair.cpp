fe8124e277a4b8ec3bd83ec4ef9589cf0f89c4a6
Rkmutech <ramkumar.iconao@gmail.com>
2025-06-10 22:20:28 +0530

    pair file

--- pair.cpp
+++ pair.cpp
@@ -1,18 +1,20 @@
-# include<iostream>
+#include <iostream>
 using namespace std;
 
-function printPairs(int arr[],n){
-    for(int i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            cout<<arr[i],arr[j]<<endl;
+void printPairs(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            cout << arr[i] << ", " << arr[j] << endl;
         }
     }
-  
 }
 
-int main(){
-    int arr = [1,2,3,4,5];
-    int n= sizeof(arr)/sizeof(int);
-    printPairs(arr,n);
-
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printPairs(arr, n);
 }
