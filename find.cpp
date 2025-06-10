bddca5d5d6bcc9ab905124cbc9e34a1e615b59da
Rkmutech <ramkumar.iconao@gmail.com>
2025-06-10 22:22:08 +0530

    format file

--- find.cpp
+++ find.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     vector<int> v = {1, 3, 6, 2, 9};
 
     // Search an element 6
     auto it = find(v.begin(), v.end(), 6);
 
-	// Print index
+    // Print index
     cout << distance(v.begin(), it);
-  
+
     return 0;
 }
